61283ca35c89d662dda60e5783fcfc948df65d81
mfikrirmdhn <mfikrirmdhn21@gmail.com>
2026-09-02 12:36:12 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by zxura on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
